50672362854a0beef61e4da1b9a378ad9ad23889
Steven Irvin <steven.irvin@gmail.com>
2017-01-17 22:17:12 -0800

    remove nettools and telnet from yum install sh

--- yum_install_salt.sh
+++ yum_install_salt.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 yum update && yum upgrade -y && yum install -y epel-release 
-yum update && yum upgrade -y && yum install -y vim gcc libffi-devel python-devel openssl-devel telnet net-tools 
+yum update && yum upgrade -y && yum install -y vim gcc libffi-devel python-devel openssl-devel 
 yum install -y python-pip git salt-master salt-minion
 pip install -U pip
 pip install -r requirements.txt
