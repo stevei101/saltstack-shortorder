3ffbb7cb9e8ec5eca871db0955df6845c93d3471
Steven Irvin <sirvin@prosper.com>
2017-01-18 06:20:21 -0800

    add bootstrap script, update sh

--- bootstrap_yum_install_salt.sh
+++ bootstrap_yum_install_salt.sh
@@ -1,16 +1,15 @@
 #! /bin/bash
 yum update && yum upgrade -y && yum install -y epel-release 
 yum update && yum upgrade -y && yum install -y vim gcc libffi-devel python-devel openssl-devel 
-yum install -y python-pip git salt-master salt-minion
+yum install -y python-pip git salt-minion
 pip install -U pip
 pip install -r requirements.txt
 firewall-cmd --permanent --add-port=4505-4506/tcp
 mkdir -p /srv
-# git clone https://github.com/stevei101/saltstack-shortorder.git
-# cd saltstack-shortorder
+git clone https://github.com/stevei101/saltstack-shortorder.git
+cd saltstack-shortorder
 cp -Rf srv /
 cp -Rf salt /etc/
 echo "master: $(hostname -i)" >> /etc/salt/minion
-systemctl restart salt-master salt-minion
-salt-key -A
-salt '*' state.highstate
+systemctl restart salt-minion
+
